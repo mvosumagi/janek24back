35f33864ebd045b0a1a004df29ab58700a919051
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-23 14:08:16 +0300

    user vist valmis

--- src/main/java/ee/janek24back/controller/user/UserController.java
+++ src/main/java/ee/janek24back/controller/user/UserController.java
@@ -1,21 +1,29 @@
 package ee.janek24back.controller.user;
 
+import ee.janek24back.controller.user.dto.UserDetailDto;
 import ee.janek24back.controller.user.dto.UserDto;
+import ee.janek24back.infrastructure.error.ApiError;
 import ee.janek24back.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
+@Validated
 
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user")
+    @GetMapping("/")
     @Operation(
             summary = "Tagastab user info UserId alusel",
             description = "Tagastab user info UserId alusel")
@@ -23,13 +31,16 @@ public class UserController {
         return userService.findUser(userId);
     }
 
-
-
-
-
-
-
-
+    @PostMapping("/")
+    @Operation(
+            summary = "Lisab useri sisestatud andmete alusel",
+            description = "Lisab useri sisestatud andmete alusel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description = "Mingi viga", content = @Content(schema = @Schema(implementation = ApiError.class)))})
+    public void createAndSaveUser(@RequestBody @Valid UserDetailDto userDetailDto) {
+       userService.createAndSaveUser(userDetailDto);
+    }
 }
 
 
--- src/main/java/ee/janek24back/persistence/user/UserMapper.java
+++ src/main/java/ee/janek24back/persistence/user/UserMapper.java
@@ -1,12 +1,11 @@
 package ee.janek24back.persistence.user;
 
-
 import ee.janek24back.controller.login.LoginResponse;
+import ee.janek24back.controller.user.dto.UserDetailDto;
 import ee.janek24back.controller.user.dto.UserDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
@@ -15,12 +14,18 @@ public interface UserMapper {
     @Mapping(source = "role.name", target = "roleName")
     LoginResponse toLoginResponse(User user);
 
-    @Mapping(source = "id",        target = "id")
     @Mapping(source = "role.id",   target = "roleId")
     @Mapping(source = "role.name", target = "roleName")
-    @Mapping(source = "username",  target = "username")
-    @Mapping(source = "status",    target = "status")
     UserDto toUserDto(User user);
 
+    @InheritConfiguration(name = "toUserDto")
+    UserDetailDto toUserDetailDto(User user);
 
+    List<UserDetailDto> toUserDetailDtos(List<User> users);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+//    @Mapping(target = "status", expression = "java(Status.ACTIVE.getCode())")
+
+    User toUser(UserDetailDto userDetailDto);
 }
