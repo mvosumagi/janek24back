310714d76eb87de515f32b1eea85caadc7f21588
Lauri <fg@hot.ee>
2025-09-23 16:17:21 +0300

    ServiceDTO

---
+++ src/main/java/ee/janek24back/controller/service/ServiceInfo.java
@@ -0,0 +1,22 @@
+package ee.janek24back.controller.service;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link ee.janek24back.persistence.service.ServiceCategory}
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ServiceInfo implements Serializable {
+    private Integer serviceId;
+    private String serviceName;
+}
---
+++ src/main/java/ee/janek24back/persistence/service/ServiceRepository.java
@@ -0,0 +1,8 @@
+package ee.janek24back.persistence.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+
+
+}
