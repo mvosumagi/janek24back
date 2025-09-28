8082d467198249bc7c9a1ff818a3f527afa436a9
Mart-Vosumagi <mvosumagi@gmail.com>
2025-09-28 17:39:08 +0300

    user i data kuvamise jaoks

--- src/main/java/ee/janek24back/persistence/usercompany/UserCompanyRepository.java
+++ src/main/java/ee/janek24back/persistence/usercompany/UserCompanyRepository.java
@@ -1,9 +1,9 @@
 package ee.janek24back.persistence.usercompany;
 
-import ee.janek24back.persistence.company.Company;
-import ee.janek24back.persistence.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserCompanyRepository extends JpaRepository<UserCompany, Integer> {
-    boolean existsByUserAndCompany(User user, Company company);
+    boolean existsByUserAndCompany(ee.janek24back.persistence.user.User user, ee.janek24back.persistence.company.Company company);
+    Optional<UserCompany> findFirstByUser_Id(Integer userId);
 }
